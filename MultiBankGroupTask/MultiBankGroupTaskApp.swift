494353388d7ad9ec4c753f8dd7147578976a197c
Nikolay Spiridonov <nnickspr@gmail.com>
2026-02-26 15:22:04 +0300

    Initial Commit

---
+++ MultiBankGroupTask/MultiBankGroupTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiBankGroupTaskApp.swift
+//  MultiBankGroupTask
+//
+//  Created by Nikolay Spiridonov on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MultiBankGroupTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
